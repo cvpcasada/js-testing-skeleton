3afe4313d3e30ee474da5ccbd4533cf2abf4fc8c
Helen Durrant <hh.durrant@gmail.com>
2014-06-12 15:07:26 +1200

    Adding a test example that uses mocha and chai

---
+++ test/test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var chai = require('chai'),
+	HelloI18n = require('app/hello-i18n'),
+
+	expect = chai.expect;
+
+describe('HelloI18n', function() {
+
+	describe('constructor', function() {
+
+		it('should have a default language', function() {
+			var helloI18n = new HelloI18n();
+			expect(helloI18n.defaultLanguage).to.equal('icelandic');
+		});
+
+		it('should set the default language if provided', function() {
+			var helloI18n = new HelloI18n({defaultLanguage: 'japanese'});
+			expect(helloI18n.defaultLanguage).to.equal('japanese');
+		});
+	});
+
+	describe('#sayHello', function() {
+
+		it('should say hello in the specified language', function() {
+			var greetings = (new HelloI18n().sayHello('spanish'));
+			expect(greetings).to.equal('Hola Mundo');
+		});
+
+		it('should use the default language if no language is passed in', function() {
+			var greetings = (new HelloI18n().sayHello());
+			expect(greetings).to.equal('Halló Heimur');
+		});
+
+		it('should throw an error if no language is passed in in strict mode', function() {
+			expect(function() {
+				(new HelloI18n({strict: true})).sayHello();
+			}).to.throw(Error);
+		});
+
+	});
+});
